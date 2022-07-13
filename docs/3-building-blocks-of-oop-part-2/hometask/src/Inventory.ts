2eb415d05e68335e080025acef831438acfdbe1c
Anna <ajustusa@gmail.com>
2022-07-13 18:54:56 +0200

    feat: add class Inventory

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Inventory.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Inventory.ts
@@ -1,1 +1,24 @@
-// your code goes here
+import { Item } from './Item';
+import { ItemComparator } from './ItemComparator';
+
+export class Inventory {
+	private items: Item[] = [];
+
+	public sort(): void;
+	public sort(comparator: ItemComparator): void;
+	public sort(comparator?: ItemComparator): void {
+		if (!comparator) {
+			this.items = this.items.sort((first, second) => first.getValue() - second.getValue());
+		} else {
+			this.items = this.items.sort((first, second) => first.getWeight() - second.getWeight());
+		}
+	}
+
+	public toString(): string {
+		return this.items.join(', ');
+	}
+
+	public addItem(item: Item): void {
+		this.items.push(item);
+	}
+}
