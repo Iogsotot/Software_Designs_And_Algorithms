ea4a5d9683631e639583f276c86bec07a19f95cc
Iogsotot <ajustusa@gmail.com>
2022-08-03 20:32:13 +0200

    feat: add different kinds of Shipment (bridge)

--- docs/5-design-patterns/hometask/Shippers/Shipper.ts
+++ docs/5-design-patterns/hometask/Shippers/Shipper.ts
@@ -1,29 +1,75 @@
 import { AirEast, ChicagoSprint, PacificParcel } from './index';
+import { ShipmentType } from '../Shipment';
 
 export interface IShipperStrategy {
-	cost: number;
+	getLetterCost: (weight: number) => number;
+	getPackageCost: (weight: number) => number;
+	getOversizedCost: (weight: number) => number;
+	getStandardPackageCost: (weight: number) => number;
 }
 
 class AirEastShipperStrategy implements IShipperStrategy {
-	cost = AirEast.cost;
+	private baseCost = AirEast.cost;
+
+	getStandardPackageCost(weight: number): number {
+		return weight * this.baseCost;
+	};
+
+	getLetterCost(weight: number): number {
+		return weight * this.baseCost;
+	};
+	getPackageCost(weight: number): number {
+		return weight * 25;
+	};
+	getOversizedCost(weight: number): number {
+		return this.getStandardPackageCost(weight) + 1000;
+	};
 }
 
 class ChicagoSprintShipperStrategy implements IShipperStrategy {
-	cost = ChicagoSprint.cost;
+	private baseCost = ChicagoSprint.cost;
+
+	getStandardPackageCost(weight: number): number {
+		return weight * this.baseCost;
+	};
+
+	getLetterCost(weight: number): number {
+		return weight * this.baseCost;
+	};
+	getPackageCost(weight: number): number {
+		return weight * 20;
+	};
+	getOversizedCost(weight: number): number {
+		return this.getStandardPackageCost(weight);
+	};
 }
 
 class PacificParcelShipperStrategy implements IShipperStrategy {
-	cost = PacificParcel.cost;
+	private baseCost = PacificParcel.cost;
+
+	getStandardPackageCost(weight: number): number {
+		return weight * this.baseCost;
+	};
+
+	getLetterCost(weight: number): number {
+		return weight * this.baseCost;
+	};
+	getPackageCost(weight: number): number {
+		return weight * 19;
+	};
+	getOversizedCost(weight: number): number {
+		return this.getStandardPackageCost(weight) + weight * 2;
+	};
 }
 
 class Shipper {
 	shipper: IShipperStrategy;
 
-	constructor(zipCode: string) {
+	constructor(zipCode: string, weight: number) {
 		this.setStrategy(zipCode[0]);
 	}
 
-	private setStrategy(zipCode: string) {
+	private setStrategy(zipCode: string): void {
 		if ([1, 2, 3].includes(+zipCode)) {
 			this.shipper = new AirEastShipperStrategy();
 		} else if ([4, 5, 6].includes(+zipCode)) {
@@ -35,7 +81,18 @@ class Shipper {
 		}
 	}
 
-	public getCost() { return this.shipper.cost };
+	public getCost(type: ShipmentType, weight: number): number {
+		if (type === ShipmentType.LETTER) {
+			return this.shipper.getLetterCost(weight);
+		} else if (type === ShipmentType.PACKAGE) {
+			return this.shipper.getPackageCost(weight);
+		}
+		else if (type === ShipmentType.OVERSIZED) {
+			return this.shipper.getOversizedCost(weight);
+		}
+		return this.shipper.getStandardPackageCost(weight);
+
+	};
 }
 
 export default Shipper;
